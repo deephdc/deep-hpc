190ac59c7cf2085ac05ef6dfe8482cc87bb1da6c
Valentin <valentin.kozlov@gmail.com>
2019-03-19 21:04:53 +0100

    Update deepaas-hpc.sh

--- deepaas-hpc.sh
+++ deepaas-hpc.sh
@@ -12,16 +12,16 @@
 
 ### <<<= SET OF USER PARAMETERS =>>> ###
 # Docker image to download and use
-DockerImage="deephdc/deep-oc-dogs_breed_det:cpu"
+DockerImage="deephdc/deep-oc-<application>"
 # Name for the container to be created (can be arbitrary name)
-ContainerName="deep-oc-dogs-cpu"
+ContainerName="<application-tag>"
 USE_GPU=false
 
 ### Matching between Host directories and Container ones
 # Comment out if not used
 #
 # Name of the corresponding Python package
-PyPackage="dogs_breed_det"
+PyPackage="<application>"
 HostData=$HOME/$PyPackage/data
 ContainerData=/srv/$PyPackage/data
 #
