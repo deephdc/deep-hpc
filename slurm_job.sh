314e2f5e029df27c1b5d537543b7601b6c24b0ef
DEEP user <valentin.kozlov@kit.edu>
2019-05-20 15:41:03 +0000

    job log rename

--- slurm_job.sh
+++ slurm_job.sh
@@ -11,7 +11,7 @@
 #####################################
 DATENOW=$(date +%y%m%d_%H%M%S)
 JOB2RUN="./deepaas-hpc.sh"
-JOBLOG="job_${DATENOW}_deepaas.log"
+JOBLOG="${DATENOW}_deepaas_job.log"
 ### EXAMPLE FOR THE PREDICTION CALL
 #JOBPARAMS="-o ${DATENOW}_deepaas.json -d St_Bernard_Dog_001.jpg"
 
