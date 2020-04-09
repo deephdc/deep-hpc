1905866949d82e2da8ac3d7161d5375ee47e102c
Valentin Kozlov <valentin.kozlov@gmail.com>
2020-04-10 00:39:58 +0200

    massive update to add onedata, add cli options, re-arrange structure

--- slurm_app.sh
+++ slurm_app.sh
@@ -25,79 +25,187 @@
 # Script full path
 # https://unix.stackexchange.com/questions/17499/get-path-of-current-script-when-executed-through-a-symlink/17500
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
-
 DATENOW=$(date +%y%m%d_%H%M%S)
 
-### SLURM CONFIG ###
+### DEPLOYMENT MAIN CONFIGURATION (User Action!)
+export DOCKER_IMAGE="deephdc/deep-oc-benchmarks_cnn:gpu"
+export UDOCKER_CONTAINER="deep-oc-benchmarks-gpu"
+export UDOCKER_RECREATE=False
+
+##... SLURM CONFIG ...##
 # For CPU: standard, For GPU: tesla
-slurm_partition="standard"
+slurm_partition="tesla"
 slurm_nodes=1
 slurm_time="12:00:00"
 slurm_tasks_per_node=8
-slurm_jobname="deep-app"
-slurm_joblog=${DATENOW}-${slurm_jobname}-log.txt
+slurm_jobname=${UDOCKER_CONTAINER}
+slurm_log_dir="${HOME}/tmp/jobs"
+slurm_joblog="${slurm_log_dir}/${DATENOW}-${slurm_jobname}.log"
 slurm_job2run="${SCRIPT_PATH}/udocker_job.sh"
-slurm_gres=""
-###
+##
 
-### DEPLOYMENT MAIN CONFIGURATION (User Action!)
-export DOCKER_IMAGE="deephdc/deep-oc-dogs_breed_det:cpu"
-export UDOCKER_CONTAINER="deep-oc-dogs_breed-cpu"
-export UDOCKER_RECREATE=false
+##... oneclient ..##
+export ONECLIENT_ACCESS_TOKEN="MDXXX"
+# options: 
+# oprov.ifca.es 
+# oneprovider.fedcloud.eu
+# cloud-90-147-75-163.cloud.ba.infn.it
+export ONECLIENT_PROVIDER_HOST="oprov.ifca.es"
+export ONEDATA_MOUNT_POINT="$HOME/datasets"
+##
 
-MOUNT_OPTIONS="-v $HOME/deep-oc-apps/dogs_breed:/mnt/dogs_breed"
-app_in_out_base_dir="/mnt/dogs_breed"
+mount_options="-v $HOME/deep-oc-apps/benchmarks_cnn:/mnt/benchmarks_cnn"
+app_in_out_base_dir="/mnt/benchmarks_cnn"
 
-num_gpus=0
-
+num_gpus=2
 flaat_disable="yes"
 
-#... rclone config ...
+##... rclone config ...##
 rclone_conf="/srv/.rclone/rclone.conf"
 rclone_vendor="nextcloud"
 rclone_type="webdav"
 rclone_url="https://nc.deep-hybrid-datacloud.eu/remote.php/webdav/"
-rclone_user="DEEP-XYXYYXYXYXYXYXYXYXY"
-rclone_password="jXYXYXYXY_XYXYXYXYXY"
+rclone_user="DEEP-XXX"
+rclone_password="jXXX"
+##
 
 #... run_command configuration ...
 export UDOCKER_RUN_COMMAND="deepaas-cli \
---deepaas_method_output=/mnt/dogs_breed/${DATENOW}-train-out.txt \
+--deepaas_method_output=/mnt/benchmarks_cnn/${DATENOW}-train-out.txt \
 train \
---num_epochs=5 \
---sys_info=True \
---network=Resnet50"
+--batch_size_per_device=32 \
+--num_epochs=0 \
+--num_gpus=${num_gpus} \
+--model=\"resnet50 (ImageNet)\" \
+--dataset=\"Synthetic data\" \
+--evaluation=true"
+##
+### END OF THE DEPLOYMENT MAIN CONFIG ###
 
-### END OF DEPLOYMENT MAIN CONFIG ###
+### USAGE and PARSE SCRIPT FLAGS ###
+function usage()
+{
+    shopt -s xpg_echo
+    echo "Usage: $0 <options> \n
+    Options:
+    -h|--help \t\t This help message
+    -c|--cmd \t\t Command to run inside the Docker image
+    -p|--partition \t Choose which cluster partition to use (e.g. CPU: 'standard', GPU: 'tesla')" 1>&2; exit 0;
+    #-g|--gpus \t\t Number of GPUs to request
+}
+
+function check_arguments()
+{
+    OPTIONS=h,c:,g:,p:
+    LONGOPTS=help,cmd:,gpus:,partition:
+    # https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+    #set -o errexit -o pipefail -o noclobber -o nounset
+    set  +o nounset
+    ! getopt --test > /dev/null
+    if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+        echo '`getopt --test` failed in this environment.'
+        exit 1
+    fi
+
+    # -use ! and PIPESTATUS to get exit code with errexit set
+    # -temporarily store output to be able to check for errors
+    # -activate quoting/enhanced mode (e.g. by writing out “--options”)
+    # -pass arguments only via   -- "$@"   to separate them correctly
+    ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        # e.g. return value is 1
+        #  then getopt has complained about wrong arguments to stdout
+        exit 2
+    fi
+    # read getopt’s output this way to handle the quoting right:
+    eval set -- "$PARSED"
+
+    if [ "$1" == "--" ]; then
+        echo "[INFO] No arguments provided, using defaults."
+    fi
+    # now enjoy the options in order and nicely split until we see --
+    while true; do
+        case "$1" in
+            -h|--help)
+                usage
+                shift
+                ;;
+            -c|--cmd)
+                export UDOCKER_RUN_COMMAND="$2"
+                shift 2
+                ;;
+            -p|--partition)
+                slurm_partition="$2"
+                shift 2
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                break
+                ;;
+            esac
+        done
+}
+
+check_arguments "$0" "$@"
+###
 
 ### NEXT IS DERIVED FROM THE OPTIONS ABOVE
-#... UDOCKER_USE_GPU ...
-export UDOCKER_USE_GPU=false
-if [ $num_gpus -gt 0 ]; then
-    export UDOCKER_USE_GPU=true
-    slurm_gres="--gres=gpu:${num_gpus}"
-    echo "[INFO] GPU usage is activated"
+if [ ! -d ${slurm_log_dir} ]; then
+    mkdir -p ${slurm_log_dir}
 fi
 
+echo "=== DATE: ${DATENOW}" >>${slurm_joblog}
+
 ### Configure UDOCKER_OPTIONS ###
-ENV_OPTIONS="-e RCLONE_CONFIG=${rclone_conf} \
+env_options=""
+
+if [ ${#rclone_user} -gt 8 ] && [ ${#rclone_password} -gt 8 ]; then
+    env_options+="-e RCLONE_CONFIG=${rclone_conf} \
 -e RCLONE_CONFIG_RSHARE_TYPE=${rclone_type} \
 -e RCLONE_CONFIG_RSHARE_URL=${rclone_url} \
 -e RCLONE_CONFIG_RSHARE_VENDOR=${rclone_vendor} \
 -e RCLONE_CONFIG_RSHARE_USER=${rclone_user} \
--e RCLONE_CONFIG_RSHARE_PASS=${rclone_password} \
--e APP_INPUT_OUTPUT_BASE_DIR=${app_in_out_base_dir}"
+-e RCLONE_CONFIG_RSHARE_PASS=${rclone_password}"
+else
+    env_options+="-e RCLONE_CONFIG=/srv/.rclone/rclone.conf"
+fi
 
-export UDOCKER_OPTIONS="${ENV_OPTIONS} ${MOUNT_OPTIONS}"
+# add app_in_out_base_dir if set
+if [ ${#app_in_out_base_dir} -gt 5 ]; then
+    env_options+=" -e APP_INPUT_OUTPUT_BASE_DIR=${app_in_out_base_dir}"
+fi
+
+# add flaat env setting
+env_options+=" -e DISABLE_AUTHENTICATION_AND_ASSUME_AUTHENTICATED_USER=$flaat_disable"
+
+export UDOCKER_OPTIONS="${env_options} ${mount_options}"
 
 ### Configure SLURM submission
 SLURM_OPTIONS="--partition=${slurm_partition} \
 --nodes=${slurm_nodes} \
 --ntasks-per-node=${slurm_tasks_per_node} \
 --time=${slurm_time} \
---job-name=${slurm_jobname} \
---output=${slurm_joblog} \
-${slurm_gres}"
+--job-name=${slurm_jobname}"
+
+#... UDOCKER_USE_GPU + slurm's --gres
+export UDOCKER_USE_GPU=false
+if [ $num_gpus -gt 0 ]; then
+    export UDOCKER_USE_GPU=true
+    SLURM_OPTIONS+=" --gres=gpu:${num_gpus}"
+    echo "=== [INFO] GPU usage is activated" >>${slurm_joblog}
+fi
+
+# add job log, if the output file is provided (longer than 5 characters)
+if [ ${#slurm_joblog} -ge 5  ]; then
+    SLURM_OPTIONS+=" --output=${slurm_joblog}"
+fi
+
+echo "=== [SLURM settings]" >>${slurm_joblog}
+echo " SLURM OPTIONS = ${SLURM_OPTIONS}" >>${slurm_joblog}
+echo "=== [/SLURM] ===" >>${slurm_joblog}
 
 # Final submission to SLURM
 sbatch ${SLURM_OPTIONS} ${slurm_job2run}
--- udocker_job.sh
+++ udocker_job.sh
@@ -11,50 +11,78 @@
 
 ###  INFO  #################################################################
 # Script that runs a DEEP-OC application via a udocker container.
-#
 # NOTE: The udocker container needs to be set up with nvidia compatibility
 ############################################################################
 
+function print_date()
+{
+    echo $(date +'%Y-%m-%d %H:%M:%S')
+}
 
+##### MOUNT ONEDATA on the HOST #####
+if [ ${#ONECLIENT_ACCESS_TOKEN} -gt 8 ] && [ ${#ONECLIENT_PROVIDER_HOST} -gt 8 ]; then
+   # check if the local mount point exists
+   if [ ! -d "${ONEDATA_MOUNT_POINT}" ]; then
+       mkdir -p ${ONEDATA_MOUNT_POINT}
+   fi
+   echo "=== [ONEDATA: $(print_date) ]"
+   echo "...ONECLIENT_PROVIDER_HOST = ${ONECLIENT_PROVIDER_HOST}"
+   echo "...ONEDATA_MOUNT_POINT = ${ONEDATA_MOUNT_POINT}"
+   oneclient --version
+   oneclient ${ONEDATA_MOUNT_POINT}
+   ls ${ONEDATA_MOUNT_POINT}
+   echo "=== [/ONEDATA] ==="
+fi
+####
 
 ##### RUN THE JOB #####
 IFContainerExists=$(udocker ps |grep "${UDOCKER_CONTAINER}")
 IFImageExists=$(echo ${IFContainerExists} |grep "${DOCKER_IMAGE}")
-if [ ${#IFImageExists} -le 1 ] || [ ${#IFContainerExists} -le 1 ] || [ "$UDOCKER_RECREATE" = true ]; then
-    udocker pull ${DOCKER_IMAGE}
+if [ ${#IFImageExists} -le 1 ] || [ ${#IFContainerExists} -le 1 ] || echo ${UDOCKER_RECREATE} |grep -iqF "true"; then
+    echo "=== [INFO: $(print_date) ]"
     if [ ${#IFContainerExists} -gt 1 ]; then
-        echo "Removing container ${UDOCKER_CONTAINER}..."
+        echo "=> Removing container ${UDOCKER_CONTAINER}..."
         udocker rm ${UDOCKER_CONTAINER}
     fi
-    echo "Creating container ${UDOCKER_CONTAINER}..."
+    udocker pull ${DOCKER_IMAGE}
+    echo "=> $(print_date): Creating container ${UDOCKER_CONTAINER}..."
     udocker create --name=${UDOCKER_CONTAINER} ${DOCKER_IMAGE}
+    echo "=> $(print_date): Created"
+    echo "=== [/INFO] ==="
 else
-    echo "=== [INFO] ==="
-    echo " ${UDOCKER_CONTAINER} already exists!"
-    echo " udocker ps: ${IFContainerExists}"
-    echo " Trying to re-use it..."
-    echo "=== [INFO] ==="
+    echo "=== [INFO: $(print_date) ]"
+    echo "=> ${UDOCKER_CONTAINER} already exists!"
+    echo "=> udocker ps: ${IFContainerExists}"
+    echo "=> Trying to re-use it..."
+    echo "=== [/INFO] ==="
 fi
 
 # if GPU is to be used, apply an 'nvidia hack'
 # and setup the container for GPU
 if echo ${UDOCKER_USE_GPU} |grep -iqF "true"; then
-    echo "Setting up Nvidia compatibility."
-    #nvidia-modprobe -u -c=0  # CURRENTLY only to circumvent a bug
+    echo "=== [NVIDIA: $(print_date) ]"
+    echo "=> Setting up Nvidia compatibility."
+    nvidia-modprobe -u -c=0  # CURRENTLY only to circumvent a bug
     udocker setup --nvidia --force ${UDOCKER_CONTAINER}
-    nvidia-smi
+    udocker run ${UDOCKER_CONTAINER} nvidia-smi
+    echo "=== [/NVIDIA] ==="
 fi
 
-echo "==================================="
+echo "=== [INFO: $(print_date) ]"
 echo "=> udocker container: ${UDOCKER_CONTAINER}"
 echo "=> Running on: $HOSTNAME"
 echo "...UDOCKER_OPTIONS = ${UDOCKER_OPTIONS}"
-echo "...UDOCKER_RUN_COMMAND = ${UDOCKER_RUN_COMMAND}"
-echo "==================================="
+echo "...UDOCKER_RUN_COMMAND = $UDOCKER_RUN_COMMAND"
+echo "=== [/INFO] ==="
 
 ### current hack to install deepaas-cli
-udocker run ${UDOCKER_CONTAINER} git clone -b deep_cli https://github.com/indigo-dc/DEEPaaS
-udocker run ${UDOCKER_CONTAINER} pip install -e /srv/DEEPaaS
+echo "=== [DEEPaaS: $(print_date) ]"
+deepaas_reinstall="rm -rf DEEPaaS; git clone -b deep_cli https://github.com/indigo-dc/DEEPaaS; pip install -e DEEPaaS"
+udocker run ${UDOCKER_CONTAINER} /bin/bash -c "${deepaas_reinstall}"
+echo "=== [/DEEPaaS] ==="
 
-echo "Running the application..."
-udocker run ${UDOCKER_OPTIONS} ${UDOCKER_CONTAINER} ${UDOCKER_RUN_COMMAND}
+echo "=== [INFO] Running the application, $(print_date)"
+#udocker run ${UDOCKER_OPTIONS} ${UDOCKER_CONTAINER} ${UDOCKER_RUN_COMMAND}
+udocker run ${UDOCKER_OPTIONS} ${UDOCKER_CONTAINER} /bin/bash <<EOF
+${UDOCKER_RUN_COMMAND}
+EOF
